bab1847d1754d6519cf759de441e85be4e3981b7
codebyomar <omarmohammed580@gmail.com>
2017-12-06 11:12:45 +0100

    new folder icon, and mouse events

---
+++ caldars_finale/src/components/contents/folders/folder/folder.vue
@@ -0,0 +1,20 @@
+<template src="./folder.html">
+  
+</template>
+
+<script>
+export default {
+  name:"folder",
+  props:["document"],
+  data (){
+    return {
+      isAttr:false,
+      selected:false
+    }
+  },
+  methods:{
+    showAttr(){ return this.isAttr = true},
+    hideAttr(){ return this.isAttr = false}
+  }
+}
+</script>
--- caldars_finale/src/components/contents/folders/folders.vue
+++ caldars_finale/src/components/contents/folders/folders.vue
@@ -3,12 +3,25 @@
 </template>
 
 <script>
+import folder from "./folder/folder"
+
 export default {
   name:"folders",
   data () {
     return {
-      documents:18
+      documents:[
+        {name:"Document cat1","amt":6},
+        {name:"my category","amt":3},
+        {name:"new category","amt":8},
+        {name:"Document_categor...","amt":0},
+        {name:"Document cat2","amt":9},
+        {name:"Document cat3","amt":7},
+        {name:"category 4","amt":78},
+        {name:"COMSOFT Proposal","amt":1}
+      ],
+      isAttr:false
     }
-  }
+  },
+  components:{folder}
 }
 </script>
